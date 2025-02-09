335dd5f3ec826b519c4b427aa86be49ff851bb4b
Ankush Choubey <ankushchoubey@outlook.com>
2025-02-10 03:17:41 +0530

    ads.txt

---
+++ src/pages/ads.txt.js
@@ -0,0 +1,3 @@
+export async function GET({ params, request }) {
+  return new Response(`google.com, pub-5553828151949683, DIRECT, f08c47fec0942fa0`);
+}
--- src/pages/ads.txt.md
+++
@@ -1,6 +0,0 @@
----
-title: 'ads'
-slug: ads.txt
----
-
-google.com, pub-5553828151949683, DIRECT, f08c47fec0942fa0
