cd9a51e65e16e19431597a1d990a80cc9844682d
Ankush Choubey <ankushchoubey@outlook.com>
2025-02-10 11:23:19 +0530

    send

--- src/pages/ads.txt.js
+++ src/pages/ads.txt.js
@@ -1,3 +1,11 @@
 export async function GET({ params, request }) {
-  return new Response(`google.com, pub-5553828151949683, DIRECT, f08c47fec0942fa0`);
+  const data = "google.com, pub-5553828151949683, DIRECT, f08c47fec0942fa0";
+  const response = new Response(data, {
+    headers: {
+      'Content-Type': 'text/plain',
+      'Content-Length': data.length,
+    },
+    status: 200,
+  });
+  return response;
 }
