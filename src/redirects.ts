83dd3a7ba6218904f826e166ec9fcd7d0ef4626f
Ankush Choubey <ankushchoubey@outlook.com>
2025-02-16 19:22:34 +0530

    more redirects

--- src/redirects.ts
+++ src/redirects.ts
@@ -7,6 +7,14 @@ const DECIDING_TO_LEARN_PROGRAMMING_TOPIC = '/software-blog/new_tech/'
 const BDD_VS_TDD = '/software-blog/bdd-vs-tdd';
 const OWN_OPINION = '/life-blog/original-opinion/';
 const PHYSICAL_EXERCISE = '/life-blog/exercise';
+const IDEAS = '/life-blog/thoughts-on-ideas';
+const ACTUALLY_SOLVE_THE_PROBLEM = '/v1/actually-solve-the-problem';
+const PALINDROME = ' /v1/valid-palindrome';
+const PERSONAL_OBSERVABILITY = '/life-blog/quantified-self';
+const LEARNING_STRATEGIES_FOR_DEVELOPMENT = '/software-blog/learn_programming';
+const LEADER = '/life-blog/leader';
+const GIVING_HELP = '/life-blog/giving_help'
+const FORCED_SALED = '/life-blog/forced_sale';
 export const redirects = {
   // source: destination
   '/tdd-post': '/tdd',
@@ -15,7 +23,7 @@ export const redirects = {
   '/when-to-log': MEANINGFUL_LOG_MESSAGES,
   '/writing-cleaner-tests-with-test-data-factories': '/software-blog/test-data-factories',
   '/test-data-factories': '/software-blog/test-data-factories',
-  '/who-is-a-leader': '/life-blog/leader',
+  '/who-is-a-leader': LEADER,
   '/writing-meaningful-log-messages/': MEANINGFUL_LOG_MESSAGES,
   '/v/rsa-algorithm': RSA,
   '/ttd': '/tdd',
@@ -34,38 +42,37 @@ export const redirects = {
   '/rsa-algorithm/': RSA,
   '/rsa-algorithm': RSA,
   '/physical-exercise': PHYSICAL_EXERCISE,
-  '/middleman': '/middleman',
-  '/marketing-money': '/marketing-money',
+  '/middleman': '/life-blog/middlemen',
+  '/marketing-money': '/life-blog/marketing-money',
   '/logging-when/': MEANINGFUL_LOG_MESSAGES,
-  '/life/ideas': '/life/ideas',
+  '/life/ideas': IDEAS,
   '/life/': '/life/',
-  '/life-blog/who-is-a-leader': '/life-blog/who-is-a-leader',
-  '/life-blog/ideas': '/life-blog/ideas',
-  '/life-blog/giving_help/': '/life-blog/giving_help/',
+  '/life-blog/who-is-a-leader': LEADER,
+  '/life-blog/ideas': IDEAS,
+  '/life-blog/giving_help/': GIVING_HELP,
   '/life-blog/exercise/': PHYSICAL_EXERCISE,
   '/life-blog/develop-your-own-opinions-and-reasonings': OWN_OPINION,
-  '/life-blog/benefits-of-exercising': '/life-blog/benefits-of-exercising',
-  '/life-blog/benefits-exercising': '/life-blog/benefits-exercising',
-  '/learning-strategies-for-development': '/learning-strategies-for-development',
-  '/images/life-blog/probabilities.png': '/images/life-blog/probabilities.png',
-  '/ideas': '/ideas',
-  '/dont-try-to-force-a-sale-on-people-who-are-not-your-user-base': '/dont-try-to-force-a-sale-on-people-who-are-not-your-user-base',
-  '/developing-better-self-awareness-with-quantifiedself-observability-for-personal-life': '/developing-better-self-awareness-with-quantifiedself-observability-for-personal-life',
-  '/developing-better-self-awareness-with-quantifiedself': '/developing-better-self-awareness-with-quantifiedself',
+  '/life-blog/benefits-of-exercising': PHYSICAL_EXERCISE,
+  '/life-blog/benefits-exercising': PHYSICAL_EXERCISE,
+  '/learning-strategies-for-development': LEARNING_STRATEGIES_FOR_DEVELOPMENT,
+  '/ideas': IDEAS,
+  '/dont-try-to-force-a-sale-on-people-who-are-not-your-user-base': FORCED_SALED,
+  '/developing-better-self-awareness-with-quantifiedself-observability-for-personal-life': PERSONAL_OBSERVABILITY,
+  '/developing-better-self-awareness-with-quantifiedself': PERSONAL_OBSERVABILITY,
   '/develop-yourown-opinions-and-reasonings': OWN_OPINION,
   '/develop-your-own-opinions-and-reasonings': OWN_OPINION,
-  '/definitions': '/definitions',
+  '/definitions': '/life-blog/definition',
   '/deciding-to-learn-a-programming-topic': DECIDING_TO_LEARN_PROGRAMMING_TOPIC,
   '/blog/writing-meaningful-log-messages': MEANINGFUL_LOG_MESSAGES,
-  '/blog/learning-strategies-for-development': '/blog/learning-strategies-for-development',
+  '/blog/learning-strategies-for-development': LEARNING_STRATEGIES_FOR_DEVELOPMENT,
   '/blog/develop-your-own-opinions-and-reasonings': OWN_OPINION,
-  '/blind-75-valid-palindrome': '/blind-75-valid-palindrome',
-  '/benefits-of-exercising': '/benefits-of-exercising',
+  '/blind-75-valid-palindrome': PALINDROME,
+  '/benefits-of-exercising': PHYSICAL_EXERCISE,
   '/bdd-vs-ui-ux-tdd-understanding-the-key-differences-for-customer-centric-problem-solving': BDD_VS_TDD,
   '/b2RhdGEtbW': '/b2RhdGEtbW',
   '/archives': '/archives',
-  '/actually-solve-the-problem': '/actually-solve-the-problem',
-  '/Ideas': '/Ideas',
+  '/actually-solve-the-problem': ACTUALLY_SOLVE_THE_PROBLEM,
+  '/Ideas': IDEAS,
    // 2025 Feb 16: Pages with 404 from Google Analytics
    
 };
